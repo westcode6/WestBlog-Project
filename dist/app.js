1d7564335aec64d6b6c0f275041a7ddf414ce7a9
God'spower <westcode6@gmail.com>
2022-10-31 20:47:37 +0100

    new new commit

--- dist/app.js
+++ dist/app.js
@@ -11,7 +11,7 @@ function showBlogs() {
     .then((res) => res.json())
     .then((posts) => {
       let blogPost;
-      let post = posts.slice(70, 100);
+      let post = posts.slice(70, 77);
       for (let i = 3; i < post.length; i++) {
         if (post[i].body.length >= 20) {
           let postBody = post[i].body.slice(0, 90);
@@ -20,7 +20,7 @@ function showBlogs() {
 
 
           <div class="w-full bg-white rounded-xl shadow-xl hover:shadow-md flex flex-col md:flex-row mt-8 transform transition ease hover:-translate-y-1">
-          <img src="./images/hero.jpg" class="w-full md:w-1/3 h-40 md:h-full object-cover object-center rounded-md" alt="" />
+          <img src="./images/hero.jpg" class="w-full md:w-1/3  object-cover object-center rounded-md" alt="" />
           <div class="px-6 md:px-10 py-4 order-last">
             <button class="bg-gray-200 text-gray-800 text-xs font-semibold px-4 py-2 rounded-md mb-4">Data Base</button>
             <a href="#" class="text-xl block font-bold hover:underline">
